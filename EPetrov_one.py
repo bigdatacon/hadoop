37cf884de1c1d3694aa35818b0dbb98bbb2036f4
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-11-28 12:21:09 +0300

    Update EPetrov_one.py

--- EPetrov_one.py
+++ EPetrov_one.py
@@ -6,4 +6,4 @@ spark = SparkSession.builder.appName("education").getOrCreate()
 
 # считываем и записываем датасет с категориями
 df = spark.read.format("parquet").load("/data/stage/epetrov/transactions.parquet")
-df.repartition(1).write.format("parquet").save("/data/stage/epetrov/transactions")
+df.repartition(1).write.format("parquet").save("/data/stage/epetrov/transactions/transactions.parquet")
